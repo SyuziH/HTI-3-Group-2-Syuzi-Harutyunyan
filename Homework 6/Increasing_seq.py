ca2279513b5bf80b325d76f540f259e9dc2547e8
SyuziH <syuziharutyunyan130@gmail.com>
2021-02-21 20:43:27 +0400

    '-'

--- Homework 6/Increasing_seq.py
+++ Homework 6/Increasing_seq.py
@@ -1,13 +1,27 @@
+def combinations(vals, size=None):
+    if size is None:
+        size = k
+    if size == 1:
+        return [[i] for i in vals]
+    res = []
+    c = combinations(vals, size - 1)
+    for i in vals:
+        for e in c:
+            tmp = [i]
+            if i <= e[0]:
+                tmp.extend(e)
+                res.append(tmp)
+
+    return res
+
+
 n = int(input())
 k = int(input())
-lis = [int(x) for x in range(1, n + 1)]
+vals = [int(x) for x in range(1, n + 1)]
+print(combinations(vals))
 
-if n == k:
-    for i in lis:
-        print(i, end='')
-print(" ")
-if k <= 2:
-    for i in range(0, len(lis)):
-        for j in range(0, n):
-            if i <= j:
-                print((k - 1) * str(lis[i]) + str(lis[j]))
+Total = 0
+lists = combinations(vals)
+for seq in lists:
+    Total += 1
+print(Total)
