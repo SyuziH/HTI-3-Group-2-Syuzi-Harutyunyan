e7cdd2e7bc88cb8c938c065b711c6d226c43549d
SyuziH <syuziharutyunyan130@gmail.com>
2021-03-03 15:25:45 +0400

    ''

--- Homework 7/Hangman.py
+++ Homework 7/Hangman.py
@@ -1,17 +1,16 @@
-print(" Welcome to the Hangman game! You have five guesses to get the answer correct, or you will loose:(")
+print(
+    " Welcome to the Hangman game! You have five guesses to get the answer correct, or you will loose:(")
 
 import random
 
 file = open("D:/HTI-3-Group-2-Syuzi-Harutyunyan/Homework 8/fruits.txt")
 file_content = file.read().splitlines()
 secret_word = random.choice(file_content)
-print(secret_word)
 
 guesses = ''
 turns = 1
 while turns < 7:
     failed = 0
-
     for char in secret_word:
         if char in guesses:
             print(char, end=" ")
